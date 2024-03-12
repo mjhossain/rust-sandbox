757bb0ec2c4475c43d76ecb70582951d87a84dde
Mohammed <mjhossainnyc@gmail.com>
2024-03-11 22:20:01 -0400

    variable shadowing

---
+++ shadow/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 50;
+    println!("value of x: {x}");
+    // value of x: 50
+    let x = "hello";
+    println!("value of x: {x}");
+    // value of x: hello
+    let x = true;
+    println!("value of x: {x}");
+    // value of x: true
+}
